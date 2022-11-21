85c620d419f0206932a3cad7f3715ecb2ad00bed
HiMemX <99434376+HiMemX@users.noreply.github.com>
2022-11-21 18:22:53 +0100

    Delete section.py

--- section.py
+++
@@ -1,7 +0,0 @@
-class section:
-    def __init__(self, offset, length, layer_count, layers):
-        self.offset = offset
-        self.length = length
-        self.layer_count = layer_count
-        
-        self.layers = layers # [table, table, ...]
