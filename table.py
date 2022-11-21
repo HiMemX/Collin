077472e74435170008e1f4090300de348a9c738c
HiMemX <99434376+HiMemX@users.noreply.github.com>
2022-11-21 18:23:09 +0100

    Delete table.py

--- table.py
+++
@@ -1,9 +0,0 @@
-class table:
-    def __init__(self, index, data, padding, entries):
-        self.data_offset = data[0]
-        self.data_length = data[1]
-        self.padding_offset = padding[0]
-        self.padding_length = padding[1]
-        self.offset = index[0]
-        self.length = index[1]
-        self.entries = entries # [[lwp, offs, length, flg, id, typ], [...], ...]
